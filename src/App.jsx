3148ded90280e135836d212e458bcff4b4ee3ddb
Gajanandyadav1 <gajanandsabalpura06@gmail.com>
2026-07-28 16:05:03 +0530

    changes values

--- src/App.jsx
+++ src/App.jsx
@@ -25,12 +25,11 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/hero" element={<HeroSection />} />
-        <Route path="/features" element={<FeaturesSection />} />
-
+        <Route path="/features" element={<FeaturesSection />} /> 
         {/* <Route path="/about" element={<About />} /> */}
         <Route path="/pricing" element={<PricingSection />} />
         <Route path="/contact" element={<ContactSection />} />
-        
+
         <Route path="/benefits" element={<BenefitsSection />} />
         <Route path="/cta" element={<CTASection />} />
         <Route path="/trust" element={<TrustSection/>} />
